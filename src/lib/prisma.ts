cc2ce0cdc2fb3847a4e8bffea73874f25e3538a1
Vardan Singhal <vardansinghal3808@gmail.com>
2026-02-23 00:44:05 +0530

    Fix Prisma client setup for production

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,12 @@
-import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({
+  adapter,
+});
 
-export default  prisma;
+export default prisma;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,5 @@
+import "dotenv/config"; 
 import app from "./app";
-import dotenv from "dotenv";
-
-dotenv.config();
-
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
