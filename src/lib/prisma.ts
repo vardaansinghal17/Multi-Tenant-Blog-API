3a26f19967acf6f620bd7d905c4ced4725cf19aa
Vardan Singhal <vardansinghal3808@gmail.com>
2026-02-23 00:52:46 +0530

    Fix Prisma version for deploy

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
-
-const prisma = new PrismaClient({
-  adapter,
-});
+const prisma = new PrismaClient();
 
 export default prisma;
